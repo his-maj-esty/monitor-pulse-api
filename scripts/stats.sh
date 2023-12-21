7c6c3b726626e0ce7bb1f3187dc0ce4c054e5c13
brihat <brihats34@gmail.com>
2023-12-21 18:40:02 +0530

    added cron job to send email notifications

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,2 +1,5 @@
-cronjob="* * * * * curl http://localhost:3000/monitor/checkStatus >> ~/Desktop/output.txt"
+cronjob="*/30 * * * * curl http://localhost:3000/monitor/checkStatus >> ~/Desktop/output.txt"
+(crontab -l ; echo "$cronjob") | crontab -
+
+cronjob2="3/30 * * * * curl http://localhost:3000/notification/sendNotifications >> ~/Desktop/output.txt"
 (crontab -l ; echo "$cronjob") | crontab -
